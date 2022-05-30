37694602817f237c89c32d9938379d054e5ff082
Adsam39 <92359357+Adsam39@users.noreply.github.com>
2022-05-30 10:10:49 +0200

    #thirdcommit

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,19 @@ Route::get('/', 'App\Http\Controllers\IndexC@index');
 // Appel méthode index du contrôleur produitC à partir de l'url :
 // http://localhost/monprojet/public/
 
-Route::get('/menu', 'App\Http\Controllers\IndexC@menu');
-Route::get('/search', 'App\Http\Controllers\IndexC@search');
-Route::get('/a_propos', 'App\Http\Controllers\IndexC@about');
-Route::get('/contact', 'App\Http\Controllers\IndexC@contact');
-Route::get('/blog', 'App\Http\Controllers\IndexC@blog');
+Route::get('menu', 'App\Http\Controllers\IndexC@menu');
+Route::get('search', 'App\Http\Controllers\IndexC@search');
+Route::get('a_propos', 'App\Http\Controllers\IndexC@about');
+Route::get('contact', 'App\Http\Controllers\IndexC@contact');
+Route::get('blog', 'App\Http\Controllers\IndexC@blog');
 
 //PRODUIT Controller
-Route::get('produit/consulter', 'App\Http\Controllers\ProduitC@all'); 
+
+Route::get('produit', 'App\Http\Controllers\ProduitC@products');
+
+Route::get('produit/consulter', 'App\Http\Controllers\ProduitC@product');
+
+//Route::get('produit/consulter', 'App\Http\Controllers\ProduitC@all'); 
 // Appel méthode all du contrôleur produitC à partir de l'url 
 // http://localhost/monprojet/public/produit/consulter
 
@@ -30,10 +35,12 @@ Route::post('produit/ajoutersave', 'App\Http\Controllers\ProduitC@newsave');
 // Appel méthode newsave du contrôleur à partir de l'url 
 //  Traitement des données du form de saisie d'un nouveau produit
 
-Route::get('liquide/ajouter', 'App\Http\Controllers\LiquideC@newliquide');  
+Route::get('liquide/ajouterliquide', 'App\Http\Controllers\LiquideC@newliquide');
 
-Route::post('liquide/ajoutersave', 'App\Http\Controllers\LiquideC@newsaveliquide');
+Route::post('liquide/ajouterliquidesave', 'App\Http\Controllers\LiquideC@newsaveliquide');
 
-//Route::get('/connexion', 'App\Http\Controllers\ConnexionC@signin');
+Route::get('liquide/ajoutergout', 'App\Http\Controllers\LiquideC@newgout');
 
-//Route::get('/inscription', 'App\Http\Controllers\ConnexionC@signup');
+Route::post('liquide/ajoutergoutsave', 'App\Http\Controllers\LiquideC@newgoutersave');
+
+Route::get('liquide/terminer', 'App\Http\Controllers\LiquideC@liquidefinal');
